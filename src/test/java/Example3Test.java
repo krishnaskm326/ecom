408fc00560757ea073e372f7c504ebf7277cdfd2
aarathana <aarathana@DESKTOP-L7E7ROD>
2023-04-04 09:49:58 +0530

    ecom001

---
+++ src/test/java/Example3Test.java
@@ -0,0 +1,14 @@
+
+
+import org.testng.annotations.Test;
+
+public class Example3Test {
+@Test
+	public void data() {
+		String BROWSER = System.getProperty("browser");
+//		String URL = System.getProperty("url");
+//		String USERNAME = System.getProperty("username");
+//		String PASSWORD = System.getProperty("password");
+//		System.out.println(BROWSER+" "+URL+" "+USERNAME+" "+PASSWORD);
+	}
+}
